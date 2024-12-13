23154e4695fc8436c02fe8a277a9e6f2405389a9
Vinícius Bonifácio Pereira <mrbonifaciobr@gmail.com>
2024-12-13 00:27:36 -0300

    feat: create new components to screen market

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,13 +1,23 @@
 import { useEffect, useState } from "react";
 import { Alert, Text, View } from "react-native";
+import MapView, { Callout, Marker } from "react-native-maps";
 
 import { api } from "@/services/api";
+import { colors, fontFamily } from "@/styles/theme"
+
 import { Categories, CategoriesProps } from "@/components/categories";
 import { PlaceProps } from "@/components/place";
 import { Places } from "@/components/places";
+import { router } from "expo-router";
 
 type MarketsProps = PlaceProps & {
+    latitude: number;
+    longitude: number;
+};
 
+const currentLocation = {
+    latitude: -23.561187293883442,
+    longitude: -46.656451388116494
 };
 
 export default function Home() {
@@ -33,7 +43,6 @@ export default function Home() {
             }
             const { data } = await api.get("/markets/category/" + category);
             setMarkets(data);
-            console.log(data)
         } catch (error) {
             console.error(error);
             Alert.alert("Locais", "Não foi possivel carregar os locais.");
@@ -55,6 +64,60 @@ export default function Home() {
                 onSelect={setCategory}
                 selected={category}
             />
+
+            <MapView
+                style={{ flex: 1 }}
+                initialRegion={{
+                    latitude: currentLocation.latitude,
+                    longitude: currentLocation.longitude,
+                    latitudeDelta: 0.01,
+                    longitudeDelta: 0.01,
+                }}
+            >
+                <Marker
+                    identifier="current"
+                    coordinate={{
+                        latitude: currentLocation.latitude,
+                        longitude: currentLocation.longitude,
+                    }}
+                    image={require("@/assets/location.png")}
+                />
+
+                {markets.map((item) => (
+                    <Marker
+                        key={item.id}
+                        identifier={item.id}
+                        coordinate={{
+                            latitude: item.latitude,
+                            longitude: item.longitude,
+                        }}
+                        image={require("@/assets/pin.png")}
+                    >
+                        <Callout onPress={() => router.navigate(`/market/${item.id}`)}>
+                            <View>
+                                <Text
+                                    style={{
+                                        fontSize: 14,
+                                        color: colors.gray[600],
+                                        fontFamily: fontFamily.medium,
+                                    }}
+                                >
+                                    {item.name}
+                                </Text>
+                                <Text
+                                    style={{
+                                        fontSize: 12,
+                                        color: colors.gray[600],
+                                        fontFamily: fontFamily.regular,
+                                    }}
+                                >
+                                    {item.address}
+                                </Text>
+                            </View>
+                        </Callout>
+                    </Marker>
+                ))}
+            </MapView>
 
             <Places data={markets} />
         </View>
--- src/components/place/index.tsx
+++ src/components/place/index.tsx
@@ -31,7 +31,7 @@ export function Place({ data, ...rest }: Props) {
                 <Text style={s.name}>
                     {data.name}
                 </Text>
-                <Text style={s.description}>
+                <Text style={s.description} numberOfLines={2}>
                     {data.description}
                 </Text>
                 <View style={s.footer}>
